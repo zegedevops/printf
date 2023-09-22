b581bcce28d103c26465e6956d1542c13a0de83d
Andre Ozeghe <andreozeghe@gmail.com>
2023-09-22 21:21:16 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * >format: format.
+ * zege
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -55,6 +56,7 @@ int _printf(const char *format, ...)
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * >buffer: Array of chars
+ * andre
  * >buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
